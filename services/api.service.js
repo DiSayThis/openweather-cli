637730bad6e98ad7605c17b21c0cc9794d20e223
Dmitriy Izotov <disaythis@gmail.com>
2023-01-26 21:27:04 +0300

    fix: create axios query, delete https query in comments

--- services/api.service.js
+++ services/api.service.js
@@ -7,26 +7,10 @@ const getWeather = async (city) => {
   if (!token) {
     throw new Error("Не задан ключ API, задайте его через команду -t [API_KEY]");
   }
-
   const { data } = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
     params: { q: city, appid: token, lang: "ru", units: "metric" },
   });
-  //   const url = new URL("https://api.openweathermap.org/data/2.5/weather");
-  //   url.searchParams.append("q", city);
-  //   url.searchParams.append("appid", token);
-  //   url.searchParams.append("units", "metric");
-  //   url.searchParams.append("lang", "ru");
-
-  //   console.log(url);
-  //   https.get(url, (response) => {
-  //     let res = "";
-  //     response.on("data", (chunk) => {
-  //       res += chunk;
-  //     });
-  //     response.on("end", () => {
-  //       console.log(res);
-  //     });
-  //   });
+  return data;
 };
 
 export { getWeather };
